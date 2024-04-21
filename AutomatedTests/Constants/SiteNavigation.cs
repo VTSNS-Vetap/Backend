163d5a62a07618ebd51855cdd5d16c02053b6a33
Ivan Tetkic <ivan.tetkic@gmail.com>
2024-04-21 14:08:42 +0200

    Add project files.

---
+++ AutomatedTests/Constants/SiteNavigation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedTests.Constants
+{
+    internal class SiteNavigation
+    {
+        public const string HomePage = "https://vtsns-vetap.github.io/vetap/";
+    }
+}
---
+++ AutomatedTests/SiteWorkingTest.cs
@@ -0,0 +1,82 @@
+using AutomatedTests.Constants;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.IE;
+
+namespace AutomatedTests
+{
+    public class Tests
+    {
+        IWebDriver driver;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new EdgeDriver();
+            driver.Navigate().GoToUrl(SiteNavigation.HomePage);
+            (driver as IJavaScriptExecutor).ExecuteScript("localStorage.clear();");
+        }
+
+        [Test]
+        public void UnauthoziedUser_NavigateToSite()
+        {
+
+            driver.Navigate().GoToUrl(SiteNavigation.HomePage);
+           
+            Assert.IsTrue(driver.FindElement(By.TagName("h4")).Text == "Neautorizovan pristup");
+        }
+
+
+        [Test]
+        public void SignInUser_CorrectCredentials()
+        {
+
+            driver.Navigate().GoToUrl(SiteNavigation.HomePage);
+            driver.FindElement(By.Name("signInBtn")).Click();
+            Task.Delay(1000).Wait();
+
+            driver.FindElement(By.Name("signInEmail")).SendKeys("admin@vetap.com");
+            driver.FindElement(By.Name("signInPassword")).SendKeys("1234");
+            driver.FindElement(By.Name("signInBtnSubmit")).Submit();
+            Task.Delay(2000).Wait();
+
+            Assert.IsNotNull(driver.FindElement(By.Id("signOutBtn")));
+        }
+
+        [Test]
+        public void SignInUser_AddWrongPassword_ShowErrorText()
+        {
+
+            driver.Navigate().GoToUrl(SiteNavigation.HomePage);
+            driver.FindElement(By.Name("signInBtn")).Click();
+            Task.Delay(1000).Wait();
+
+            driver.FindElement(By.Name("signInEmail")).SendKeys("admin@vetap.com");
+            driver.FindElement(By.Name("signInPassword")).SendKeys("12345");
+            driver.FindElement(By.Name("signInBtnSubmit")).Submit();
+            Task.Delay(1000).Wait();
+
+            Assert.IsNotNull(driver.FindElement(By.Id("signOutBtn")));
+        }
+
+
+
+        [OneTimeTearDown]
+        public void OnTimeTearDown()
+
+        {
+            driver.Quit();
+           
+        }
+
+        [TearDown]
+        public void TearDown()
+
+        {
+
+            driver.Dispose();
+        }
+    }
+}
